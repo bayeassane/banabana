f00f45197e83f58711f0594091d823c833515d26
Side SARR <side.sarr@uadb.edu.sn>
2019-11-17 12:58:22 +0000

    installable

--- angularapp/ngsw-worker.js
+++ angularapp/ngsw-worker.js
@@ -2745,3 +2745,29 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
     const driver = new Driver(scope, adapter, new CacheDatabase(scope, adapter));
 
 }());
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+	e.preventDefault();
+	//On récupère l’évènement pour l'utiliser plus tard
+	deferredPrompt = e;
+
+	//On affiche le bouton d'installation
+	document.getElementById('btnInstall').style.display = 'block';
+});
+
+document.getElementById('btnInstall').addEventListener('click', (e) => {
+	document.getElementById('btnInstall').style.display = 'none';
+	// Affiche la demande d’installation 
+	deferredPrompt.prompt();
+
+	deferredPrompt.userChoice.then((choiceResult) => {
+		if (choiceResult.outcome === 'accepted') {
+			console.log('Installation réussie');
+		} else {
+			console.log('Installation refusée');
+		}
+		deferredPrompt = null;
+	});
+});
